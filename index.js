67c028cf0ef451e60fb1cd3b199a8248eb4f15f8
Tugbish <tugba.kursun@yahoo.com.tr>
2021-12-23 13:09:02 +1100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let retVal=[];
+    let i = 0;
+    while (i < names.length) {
+        retVal.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++;
+    }
+    return retVal;
+}
+
+let countDown = function(i) {
+    console.log(i);
+    i>0 && countDown(i-1);
+}
+    countDown(i);
+
